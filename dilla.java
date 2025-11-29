f567306a5163b970e28d706967aa6678640cf631
weekfive <np01cp4a250009@islingtoncollege.edu.np>
2025-11-29 20:28:51 +0545

    Initial sharing of project

---
+++ dilla.java
@@ -0,0 +1,12 @@
+
+/**
+ * Write a description of class dilla here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class dilla
+{
+    public static void main (String[] args){
+    System.out.println("hii");
+}}
